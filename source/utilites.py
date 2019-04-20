a12f3a323dd15ba59d47982ce5ec658e8b123347
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2019-04-20 12:55:34 +0300

    изменён алгоритм генерации обыкновенных дробей. теперь целое число для знаменателя генерируется только один раз, а в случае если оно будет равно нулю, к знаменателю прибавляется еденица.

--- source/utilites.py
+++ source/utilites.py
@@ -9,7 +9,6 @@ def generate_integer(rng):
 	return random.randint(*rng)
 
 
-
 def generate_decimal(rng):
 	decimal_places = random.choice([1, 2])
 	float_num =round(random.uniform(*rng), decimal_places)
@@ -19,7 +18,6 @@ def generate_decimal(rng):
 def generate_fraction(rng):
 	if rng[0] > rng[1]: rng.reverse()
 	numerator =generate_integer(rng)
-	while True:
-		denominator =generate_integer(rng)
-		if denominator != 0: break
+	denominator =generate_integer(rng)
+	if denominator == 0: denominator += 1
 	return fractions.Fraction(numerator, denominator)
