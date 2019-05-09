2e87bea71c66767278a6e6d0716cebda7d894bdf
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2019-05-09 12:42:43 +0300

    определён словарь, содержащий арифметические операторы, их преоритеты и действия. каждому оператору соответствует кортеж, содержащий преоритет операции и lambda функцию, определяющую действие этого оператора над двумя операндами.

--- source/learning/defines.py
+++ source/learning/defines.py
@@ -1,1 +1,10 @@
-﻿
+﻿ARITHMETIC_OPERATORS = {
+	'+': (2, 
+		lambda a, b: a + b), 
+	'-': (2, 
+		lambda a, b: a - b), 
+	'*': (1, 
+		lambda a, b: a * b), 
+	'/': (1, 
+		lambda a, b: a // b if isinstance(a, int) and isinstance(b, int) else a / b)
+}
