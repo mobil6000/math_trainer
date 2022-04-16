276d92da2d59383496bb77ac2eaf832570cb9900
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-04-16 21:43:27 +0300

    Add type hints to object which implements time measurement. also, increase its performance

--- source/utilites.py
+++ source/utilites.py
@@ -2,22 +2,29 @@
 import fractions
 import random
 import time
+from typing import Optional
 
 
 
 class TimeMeter:
 
-	def __init__(self):
+	__slots__ = ('start_time', 'end_time')
+	start_time: Optional[float]
+	end_time: Optional[float]
+
+	def __init__(self) -> None:
 		self.__start_time, self.__end_time = None, None
 
 
-	def start(self):
-		if self.__end_time is not None: self._end_time = None
+	def start(self -> None):
+		if self.__end_time is not None:
+			self._end_time = None
 		self.__start_time = time.monotonic()
 
 
-	def finish(self):
-		if self.__start_time is None: raise NotImplementedError('start point of time measurement is not defined')
+	def finish(self) -> int:
+		if self.__start_time is None:
+			raise NotImplementedError('start point of time measurement is not defined')
 		self.__end_time = time.monotonic()
 		return int(self.__end_time - self.__start_time)
 
