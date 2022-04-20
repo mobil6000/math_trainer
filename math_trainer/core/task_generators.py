369a8dd74e939c99215d24d4053952301d3f357b
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2022-04-20 23:22:01 +0300

    Add a docstrings for an arithmetic task generator class

--- math_trainer/core/task_generators.py
+++ math_trainer/core/task_generators.py
@@ -8,6 +8,9 @@ from .import utilites
 
 
 class MathTask:
+    '''
+    Base class for all generators of mathematical exercises
+    '''
 
     def __init__(self) -> None:
         self.__current_expression = self._generate_expression()
@@ -20,17 +23,28 @@ class MathTask:
 
     @abstractmethod
     def _generate_expression(self) -> str:
+        '''
+        implements an algorithm for generating mathematical expressions.
+        this method must be overridden in child classes.
+        '''
         pass
 
 
     @abstractmethod
     def check_result(self, expected_result: str) -> bool:
+        '''
+        Checks correctness of the passed result of the expression calculation.
+        this method must be overridden in child classes.
+        '''
         pass
 
 
 
 @final
 class ArithmeticTask(MathTask):
+    '''
+    Implements generator of arithmetic tasks
+    '''
 
     __NUMBER_TYPES = ('integer', 'decimal',)
     __OPERATORS = ('+', '-', '*', '/',)
@@ -41,8 +55,10 @@ class ArithmeticTask(MathTask):
 
 
     def __get_number_type(self) -> str:
-        if self.__number_type is not None: return self.__number_type
-        else: return random.choice(self.__NUMBER_TYPES)
+        if self.__number_type is not None:
+            return self.__number_type
+        else:
+            return random.choice(self.__NUMBER_TYPES)
 
 
     def __get_operator(self) -> str:
@@ -57,6 +73,9 @@ class ArithmeticTask(MathTask):
 
 
     def _generate_expression(self) -> str:
+        '''
+        Generates an arithmetic expression.
+        '''
         template = '{0} {1} {2} =?'
         operand1 = self.__get_number(self.__get_number_type())
         operand2 = self.__get_number(self.__get_number_type())
@@ -65,5 +84,8 @@ class ArithmeticTask(MathTask):
 
 
     def check_result(self, expected_result: str) -> bool:
+        '''
+        Checks correctness of the passed result of the arithmetic expression calculation
+        '''
         real_result = eval(self.__current_expression[:-3])
         return int(expected_result) == real_result
